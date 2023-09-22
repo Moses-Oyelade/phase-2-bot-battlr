da43840b346ceb8581285d8418a50a91457de85f
Moses-Oyelade <oyelademoses@gmail.com>
2023-09-22 12:07:14 +0100

    Components set-up

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+// import './App.css';
+
+
+
+
+function App() {
+
+
+
+  return (
+    <>
+      <BrowserRouter>
+          <Navbar/>
+          <Routes>
+            <Route/>
+            <Route/>
+          </Routes>
+      </BrowserRouter>
+    </>
+   
+  );
+}
+
+export default App;
---
+++ src/Navbar.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { NavLink } from "react-router-dom"
+
+function Navbar() {
+
+
+
+  return (
+    <div>
+        <nav>
+            <ul>
+                <li>
+                    <NavLink to="/">Home</NavLink>
+                </li>
+                <li>
+                    <NavLink to="/botcollection">BotCollection</NavLink>
+                </li>
+                <li>
+                    <NavLink to="myarmybot">MyArmyBot</NavLink>
+                </li>
+            </ul>
+        </nav>
+    </div>
+  )
+}
+
+export default Navbar
